85808030beea9f064cdc452ce85e9c12e9b7a6f8
Blade Pan <blade@msn.cn>
2020-11-01 15:39:02 -0800

    make SystemClock constructor internal

--- Nier.Commons/SystemClock.cs
+++ Nier.Commons/SystemClock.cs
@@ -7,6 +7,12 @@ namespace Nier.Commons
     /// </summary>
     public class SystemClock : ISystemClock
     {
+        public static readonly SystemClock Instance = new SystemClock();
+
+        private SystemClock()
+        {
+        }
+
         public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
     }
 }
